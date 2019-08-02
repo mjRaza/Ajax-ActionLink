17f0082a3cb52f2d61035c57e3c73fd662371e11
Taha Muneeb <taha.muneeb@irsavideo.com>
2019-08-02 15:16:54 +0500

    Learning Ajax for Asp.Net

---
+++ AjaxActionLink/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AjaxActionLink.Models;
+
+namespace AjaxActionLink.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        EmployeeDBEntities db = new EmployeeDBEntities();
+
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AllEmployee()
+        {
+           var data= db.EmployeeTable.ToList();
+            return PartialView("_AllEmployee",data);
+        }
+
+
+
+
+
+
+    }
+}
